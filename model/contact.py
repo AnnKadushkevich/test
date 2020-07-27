917d70bbec3bfdb043ae0c11f2624f6b455e9948
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-07-27 20:21:29 +0300

    Update contact.py

--- model/contact.py
+++ model/contact.py
@@ -2,12 +2,12 @@ from sys import maxsize
 class Contact:
 
     def __init__(
-            self,firstname,middlename,
-            lastname,nickname,title,company,
-            address,home,mobile,work,
-            fax,email,email2,email3,
-            homepage,address2,phone2,
-            notes, id
+            self,firstname=None,middlename=None,
+            lastname=None,nickname=None,title=None,company=None,
+            address=None,home=None,mobile=None,work=None,
+            fax=None,email=None,email2=None,email3=None,
+            homepage=None,address2=None,phone2=None,
+            notes=None, id=None
     ):
         self.firstname = firstname
         self.middlename = middlename
