ed5e12fbbc87df2b764df926e02271dba1f5f475
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-07-27 22:21:48 +0300

    Update test_del_contact.py

--- tests/test_del_contact.py
+++ tests/test_del_contact.py
@@ -1,12 +1,14 @@
 
 from model.contact import Contact
+from random import  randrange
 
-def test_delete_contact(app):
+def test_delete_some_contact(app):
     if app.contacts.count() == 0:
         app.contacts.create(Contact(firstname='test'))
     old_contact = app.contacts.get_contact_list ()
-    app.contacts.delete_first_contact()
+    index = randrange ( len ( old_contact ) )
+    app.contacts.delete_contact_by_index(index)
     new_contact = app.contacts.get_contact_list ()
     assert len ( old_contact - 1 ) == len ( new_contact )
-    old_contact[0:1] = []
+    old_contact[index:index+1] = []
     assert old_contact == new_contact
