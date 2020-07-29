bb0f82a7637b031a2d1502b11d6867cb3ca09240
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-07-29 22:06:45 +0300

    Update test_modify_contacts.py

--- tests/test_modify_contacts.py
+++ tests/test_modify_contacts.py
@@ -4,12 +4,12 @@ from random import randrange
 
 def test_modify_contact(app):
     if app.contact.count() == 0:
-        app.contacts.contact(Contact(firstname="add"))
-    old_contacts = app.contacts.get_contact_list()
+        app.contact.contact(Contact(firstname="add"))
+    old_contacts = app.contact.get_contact_list()
     index = randrange(len(old_contacts))
     contact = Contact(firstname="add")
     contact.id = old_contacts[index].id
-    app.contacts.modificate_contact_by_index(index, contact)
+    app.contact.modificate_contact_by_index(index, contact)
     new_contacts = app.contact.get_contact_list()
     assert len(new_contacts) == len(old_contacts)
     old_contacts[index] = contact
