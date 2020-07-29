18634406f1ed472c86fc7702038e3f0452e4b11f
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-07-29 21:55:27 +0300

    Update contact.py

--- model/contact.py
+++ model/contact.py
@@ -1,23 +1,42 @@
 from sys import maxsize
 class Contact:
 
-    def __init__(self,firstname=None,lastname=None,homephone=None,mobilephone=None,workphone=None,
-            secondaryphone=None,id=None):
+    def __init__(self, firstname=None, lastname=None, nickname=None, title=None, company=None,
+                 address=None, homephone=None, mobilephone=None, workphone=None,
+                 fax=None, email=None, email2=None,  email3=None, homepage=None, address2=None, secondaryphone=None, notes=None, id=None, all_phones_from_home_page=None,
+                 bday=None, bmonth=None, byear=None, aday=None, amonth=None, ayear=None):
 
         self.firstname = firstname
         self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
         self.homephone = homephone
         self.mobilephone = mobilephone
         self.workphone = workphone
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
         self.secondaryphone = secondaryphone
+        self.notes = notes
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
-        return "%s:%s %s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s;%s;%s;%s;%s" % (self.id, self.firstname, self.lastname, self.address, self.email, self.home)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id)\
-               and self.firstname == other.firstname and self.lastname == other.lastname
+        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
 
     def id_or_max(self):
         if self.id:
