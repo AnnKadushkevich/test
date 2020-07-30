2d4d107ca65f2998c486f26720993421a183af64
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-07-30 21:18:35 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,12 +6,14 @@ fixture = None
 @pytest.fixture
 def app (request):
     global fixture
+    browser = request.config.getoption ( "--browser" )
+    base_url = request.config.getoption ( "--baseUrl" )
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
-        if not fixture is_valid():
-            fixture = Application ()
-    fixture.session.ensure_login ( username="admin", password="secret" )
+        if not fixture.is_valid():
+            fixture = App(browser=browser, base_url=base_url)
+    fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
 
@@ -20,6 +22,10 @@ def stop (request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
-
-    request.addfinalizer(fin)
+    request.addfinalizer ( fin )
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption ( "--browser", action="store", default="firefox" )
+    parser.addoption ( "--baseUrl", action="store", default="http://localhost/addressbook/" )
