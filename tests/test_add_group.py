11049b7d2f5ec99c0785e88e2fd77a37feb3f1ad
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-08-01 23:29:34 +0300

    Update test_add_group.py

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,28 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from model.group import Group
-import pytest
-import random
-import  string
 
-def radom_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata = [Group ( name="", header="", footer="" )] + [
-    Group ( name = radom_string("name",10), header = radom_string("header",20), footer=radom_string("footer",20))
-    for i in range(5)
-
-]
-
-@pytest.mark.paramitrize("group",testdata,ids=[repr (x) for x in testdata])
-def test_add_group(app,group):
+def test_add_group(app,json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len (old_groups + 1) == app.group.count()
     new_groups = app.group.get_group_list ()
     old_groups.append(group)
     assert sorted(old_groups, key= Group.id_or_max) == sorted(new_groups,key=Group.id_or_max)
+
 
 
 
