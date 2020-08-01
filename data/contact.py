7d31991c3f97c016b9380b35f8b7e8ddb7d2b322
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-08-01 23:17:29 +0300

    Create contact.py

---
+++ data/contact.py
@@ -0,0 +1,7 @@
+from model.contact import Contact
+
+constant = [
+    Contact(firstname="firstname1", lastname="lastname1", address="address1", mobilephone="1", workphone="1",
+               email="email"),
+    Contact(firstname="firstname2", lastname="lastname2", address="address2", mobilephone="2", workphone="2",
+               email="email2")
