d72bc5f85e44beed970d90ba8698e5b45e36cfef
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-08-01 20:17:16 +0300

    Delete test_edit_contact.py

--- tests/test_edit_contact.py
+++
@@ -1,7 +0,0 @@
-from model.contact import Contact
-
-def test_edit_contact(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname=''))
-
-    app.contact.edit_contact()
