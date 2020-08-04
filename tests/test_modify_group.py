e37efd8f610e05702833f69a28499b04daf0e1b9
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-08-04 17:25:00 +0300

    Update test_modify_group.py

--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -1,23 +1,33 @@
+# -*- coding: utf-8 -*-
+import random
 
+import pytest
+
+from fixture import db
 from model.group import Group
-from random import randrange
-
-def test_modify_group_name(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name = "test"))
-    old_groups = app.group.get_group_list ()
-    index = randrange ( len ( old_groups ) )
-    group = Group(name="New group")
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(index, group)
-    new_groups = app.group.get_group_list ()
-    assert len ( old_groups) == len ( new_groups )
-    old_groups[index] = group
-    assert sorted ( old_groups, key=Group.id_or_max ) == sorted ( new_groups, key=Group.id_or_max )
+import random
 
 
-#def test_modify_group_header(app):
-#    old_groups = app.group.get_group_list ()
-#    app.group.modify_first_group(Group(header="New header"))
-#    new_groups = app.group.get_group_list ()
-#    assert len ( old_groups ) == len ( new_groups )
+def test_modificate_group_name(app, db, check_ui):
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name="test"))
+    old_groups = db.get_group_list()
+    group_old = random.choice(old_groups)
+    id = group_old.id
+    group_new = Group(id=id, name="test", header="test", footer="test")
+    app.group.modificate_group_by_id(id, group_new)
+    new_groups = db.get_group_list()
+    for i in range(len(old_groups)):
+        if old_groups[i].id == id:
+            old_groups[i] = group_new
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
+#def test_modificate_group_header(app):
+ #   if app.group.count() == 0:
+  #      app.group.create(Group(name="test"))
+   # old_groups = app.group.get_group_list()
+    #app.group.modificate_first_group(Group(header="666"))
+    #new_groups = app.group.get_group_list()
+    #assert len(old_groups) == len(new_groups)
