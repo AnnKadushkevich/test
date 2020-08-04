1bfd433a15fd357ed886ebbd2efaba425d55d67a
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-08-04 17:15:46 +0300

    Update test_add_contacts.py

--- tests/test_add_contacts.py
+++ tests/test_add_contacts.py
@@ -3,13 +3,11 @@ from model.contact import Contact
 from data.contacts import constant as testdata
 
 
-
-def test_add_contact(app, json_contact):
+def test_add_contact(app,db, json_contact):
     contact = json_contact
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     app.contact.contact(contact)
     assert len(old_contacts) + 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
