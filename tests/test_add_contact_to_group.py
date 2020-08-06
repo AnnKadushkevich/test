7a918897cb4d2970fa27c414c5357fa513bdbe02
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-08-06 20:10:39 +0300

    Update test_add_contact_to_group.py

--- tests/test_add_contact_to_group.py
+++ tests/test_add_contact_to_group.py
@@ -3,28 +3,36 @@ from model.contact import Contact
 from model.group import Group
 from fixture import orm
 
-
-
-
 orm = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
-
 
 
 def test_add_contact_to_group(app, db):
      groups = db.get_group_list()
      contacts = db.get_contact_list()
+
      if len(groups) == 0:
          app.group.create(Group(name="test"))
+
      if len(contacts) == 0:
          app.contact.contact(Contact(firstname="test"))
+
      contacts_without_groups = orm.contacts_without_groups()
      groups_without_contacts = orm.groups_without_contacts()
+
      if len(groups_without_contacts) == 0:
          app.group.create(Group(name="test"))
+         groups_without_contacts = orm.groups_without_contacts ()
+
      if len(contacts_without_groups) == 0:
          app.contact.contact(Contact(firstname="test"))
+         contacts_without_groups = orm.contacts_without_groups ()
+         
+     # добавление первого свободного контакта в первую свободную группу
      app.contact.add_contact_to_group(contacts_without_groups[0], groups_without_contacts[0])
+
      new_contacts_without_groups = orm.contacts_without_groups()
      new_groups_without_contacts = orm.groups_without_contacts()
+
+     # проверяем, что список свободных групп и контактов изменился на 1
      assert len(contacts_without_groups) == len(new_contacts_without_groups) + 1
      assert len(groups_without_contacts) == len(new_groups_without_contacts) + 1
