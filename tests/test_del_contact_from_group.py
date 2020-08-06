ab1fa9f736e533ebc42a3547cde6bc30eb8ab1bf
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-08-06 20:40:41 +0300

    Update test_del_contact_from_group.py

--- tests/test_del_contact_from_group.py
+++ tests/test_del_contact_from_group.py
@@ -5,20 +5,29 @@ from model.contact import Contact
 orm = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 def test_del_contact_from_group(app, db):
+
     groups = db.get_group_list()
     contacts = db.get_contact_list()
+
     if len(groups) == 0:
-        app.group.create(Group(name="test"))
+        app.group.create ( Group ( name="group_to_delete" ) )
+
     if len(contacts) == 0:
-        app.contact.contact(Contact(firstname="test"))
+        app.contact.contact(Contact(firstname="contact_to_delete"))
+
     contacts_in_groups = orm.contacts_in_groups()
+
     if len(contacts_in_groups) == 0:
-         group_n_contact = app.group.create(Group(name="test"))
-         contact_n_group = app.contact.contact(Contact(firstname="test"))
-         app.contact.add_contact_to_group(contact_n_group, group_n_contact)
+         group_in_contact = app.group.create(Group( name="group_to_delete))
+         contacts_in_groups = app.contact.contact(Contact(firstname="contact_to_delete"))
+         app.contact.add_contact_to_group(contacts_in_groups, group_in_contact)
          contacts_in_groups = orm.contacts_in_groups()
-    contact_n_group = contacts_in_groups[0]
-    group_n_contact = orm.get_groups_of_contact(contact_n_group)[0]
-    app.contact.remove_contact_from_group(contact_n_group, group_n_contact)
+
+    contacts_in_groups = contacts_in_groups[0]
+    group_in_contact = orm.get_groups_of_contact(contacts_in_groups)[0]
+
+    app.contact.remove_contact_from_group(contacts_in_groups, group_in_contact)
     new_contacts_in_groups = orm.contacts_in_groups()
-    assert len(contacts_in_groups) - 1 == len(new_contacts_in_groups)
+
+    # проверяем, что список свободных контактов изменился на 1
+    assert len(contacts_in_groups)  == len(new_contacts_in_groups)+1
